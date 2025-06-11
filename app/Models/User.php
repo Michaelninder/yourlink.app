89dc2940924a44a4e5d01121e1b2aa9cd7222ffc
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-11 20:45:08 +0200

    Create User.php

---
+++ app/Models/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasUuids;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'uuid',
+        'email',
+        'discord_id',
+        'password',
+        'role',
+        'plan',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function links()
+    {
+        return $this->hasMany(Link::class);
+    }
+}
