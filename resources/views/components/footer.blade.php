12ac4b7d2af65ef7eac36a0ef4f7ffc3d4a4b566
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-11 20:54:03 +0200

    Create footer.blade.php

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t mt-12 py-4">
+    <div class="text-center text-sm text-gray-500">
+        &copy; {{ now()->year }} YourLink. All rights reserved.
+    </div>
+</footer>
