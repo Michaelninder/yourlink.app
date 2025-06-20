f7cba7f0acfe0b2d6901738f2da0d42dbd942e29
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-06-20 16:18:17 +0200

    DashboardController Overhaul

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,23 +10,46 @@ use App\Models\View;
 
 class DashboardController extends Controller
 {
-    public function user()
+    public function overview()
     {
         $user = Auth::user();
 
-        return view('dashboard.user', [
+        return view('dashboard.overview', [
             'user' => $user,
-            'links_count' => $user->links()->count() ?? 0,
-            'views_count' => $user->views()->count() ?? 0,
+            'links_count' => $user->links()->count(),
+            'views_count' => $user->views()->count(),
         ]);
     }
 
-    public function admin()
+    public function overviewAdmin()
     {
-        return view('dashboard.admin', [
+        return view('admin.overview', [
             'users_count' => User::count(),
             'links_count' => Link::count(),
             'views_count' => View::count(),
+        ]);
+    }
+
+    public function links()
+    {
+        $user = Auth::user();
+
+        return view('dashboard.links', [
+            'user' => $user,
+            'links_count' => $user->links()->count(),
+            'views_count' => $user->views()->count(),
+            'links' => $user->links()->with('views')->paginate(8),
+        ]);
+    }
+
+    public function linksAdmin()
+    {
+        return view('admin.links', [
+            'users' => User::all(),
+            'links_count' => Link::count(),
+            'views_count' => View::count(),
+            'links' => Link::with('user', 'views')->paginate(10),
+            'views' => View::latest()->take(20)->get(),
         ]);
     }
 }
